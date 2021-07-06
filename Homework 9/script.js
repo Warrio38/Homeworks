f54dc0a579a974abe2753673d0e2afb8c6bf408a
Warrio38 <81041349+Warrio38@users.noreply.github.com>
2021-07-06 20:51:27 +0300

    Homework №9

--- Homework 9/script.js
+++ Homework 9/script.js
@@ -1,7 +1,7 @@
 const obj = {name: 'Alina', age: 23, adress: { country: 'UA', city: 'Kyiv'}} 
 function copy(object) {
   let retObj = {};
-  for(let objectKeys in obj){
+  for(let objectKeys in object){
     retObj[objectKeys] = object[objectKeys];
   }
   return retObj;
