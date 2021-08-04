20b7378c8cc7903643ac5b527c292489fb81df8c
Warrio38 <81041349+Warrio38@users.noreply.github.com>
2021-08-04 15:05:47 +0300

    Homework 16

--- Homework 16/script.js
+++ Homework 16/script.js
@@ -35,26 +35,27 @@ function Client(name) {
 }
 Employee.prototype = new Person();
 Client.prototype = new Person();
-const personJohn2 = new Person("John");
+const personJohn2 = new Person( "John");
 const sysAdmin2 = new Employee("Bob");
 const clientNatalia2 = new Client("Natalia");
 personJohn2.sayHello();
 sysAdmin2.sayHello();
 clientNatalia2.sayHello();
-function Student(name, marks) {
-  let Student = {
-    name: name,
-    marks: marks,
-    averageMark: function () {
-      let sum = marks.reduce((number, currentValue) => number + currentValue);
-      let result = sum / marks.length;
-      return result;
-    },
-  };
-  return Student;
-}
+function Student(name, marks){
+  this.student = function(){
+      const objStudent = {
+      name: name,
+      marks: marks,
+      averageMark: function(){
+        const result = this.marks.reduce((number, currentValue) => number + currentValue) / this.marks.length;
+        return result
+      }
+    }
+    return objStudent
+  }
+};
 const students = [
   new Student("Student 1", [10, 9, 8, 0, 10]), // имя, оценки
   new Student("Student 12", [10, 0, 8, 0, 3, 4]),
 ];
-console.log(students[0].averageMark());
+console.log(students[0].student().averageMark());
