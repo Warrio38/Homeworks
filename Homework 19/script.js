d652de2541168fbfe326736afed47a687bdeee34
Warrio38 <81041349+Warrio38@users.noreply.github.com>
2021-08-14 16:07:17 +0300

    Homework 19

---
+++ Homework 19/script.js
@@ -0,0 +1,18 @@
+let dates = new Date();
+function separation(str) {
+  str = String(str);
+  str = str.split("");
+  if (str.length === 1) {
+    str.unshift("0");
+    str = str.join("");
+  } else {
+    str = str.join("");
+  }
+  return str;
+}
+let date = `${separation(dates.getDate())}.${separation(dates.getMonth() + 1)}.${dates.getFullYear()}`;
+let time = `${separation(dates.getHours())}:${separation(dates.getMinutes())}:${separation(dates.getSeconds())}`;
+let clocks = document.getElementsByClassName('clock')
+let calendar = document.getElementsByClassName('calendar')
+clocks[0].append(time)
+calendar[0].append(date)
