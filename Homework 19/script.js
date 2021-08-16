1cf5561b0b17a9286ada48565db8e2f761aad7f1
Warrio38 <81041349+Warrio38@users.noreply.github.com>
2021-08-16 12:31:28 +0300

    Homework 19

--- Homework 19/script.js
+++ Homework 19/script.js
@@ -10,9 +10,25 @@ function separation(str) {
   }
   return str;
 }
-let date = `${separation(dates.getDate())}.${separation(dates.getMonth() + 1)}.${dates.getFullYear()}`;
-let time = `${separation(dates.getHours())}:${separation(dates.getMinutes())}:${separation(dates.getSeconds())}`;
-let clocks = document.getElementsByClassName('clock')
-let calendar = document.getElementsByClassName('calendar')
-clocks[0].append(time)
-calendar[0].append(date)
+let date = `${separation(dates.getDate())}.${separation(
+  dates.getMonth() + 1
+)}.${dates.getFullYear()}`;
+let time = `${separation(dates.getHours())}:${separation(
+  dates.getMinutes()
+)}:${separation(dates.getSeconds())}`;
+let clocks = document.getElementsByClassName("clock");
+let calendar = document.getElementsByClassName("calendar");
+clocks[0].append(time);
+calendar[0].append(date);
+function rest() {
+  dates = new Date();
+  date = `${separation(dates.getDate())}.${separation(
+    dates.getMonth() + 1
+  )}.${dates.getFullYear()}`;
+  time = `${separation(dates.getHours())}:${separation(
+    dates.getMinutes()
+  )}:${separation(dates.getSeconds())}`;
+  clocks[0].innerHTML = time;
+  calendar[0].innerHTML = date;
+}
+let interval = setInterval(rest, 1000)
