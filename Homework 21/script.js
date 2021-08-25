70a5682e073689419b40bf318cde4dcf0fa1b03c
Warrio38 <81041349+Warrio38@users.noreply.github.com>
2021-08-25 19:23:42 +0200

    Homework 21

--- Homework 21/script.js
+++ Homework 21/script.js
@@ -1,29 +1,28 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then(toObject);
-const todos = []
-function toObject(data){
-  for(let i = 0; i < data.length; i++){
-    todos.push(data[i])
+const todos = [];
+function toObject(data) {
+  for (let i = 0; i < data.length; i++) {
+    todos.push(data[i]);
   }
 }
-let newTr = document.createElement('tr')
-let newTd = document.createElement('td')
-let TBody = document.getElementById('tb')
-function allTodo(){
-  for(let i = 0; i < todos.length; i++){
-    newTd.append(todos[i].title)
-    newTr.append(newTd)
-    if(todos[i].completed === false){
-      newTr.className = 'unConfirmed'
+let newTr = document.createElement("tr");
+let newTd = document.createElement("td");
+const TBody = document.getElementById("tb");
+function allTodo() {
+  for (let i = 0; i < todos.length; i++) {
+    newTd.append(todos[i].title);
+    newTr.append(newTd);
+    if (todos[i].completed === false) {
+      newTr.className = "unConfirmed";
+    } else {
+      newTr.className = "confirmed";
     }
-    else{
-      newTr.className = 'confirmed'
-    }
-    newTr.setAttribute('data-id', todos[i].id)
-    TBody.append(newTr)
-    newTr = document.createElement('tr')
-    newTd = document.createElement('td')
+    newTr.setAttribute("data-id", todos[i].id);
+    TBody.append(newTr);
+    newTr = document.createElement("tr");
+    newTd = document.createElement("td");
   }
-  document.getElementById('button').remove()
+  document.getElementById("button").remove();
 }
